d5901a9c6b9648c117b4eb87dad77b0069d44d52
saran-raj007 <k.saranraj000@gmail.com>
2023-08-01 19:09:04 +0530

    home page update

--- frontend/home.php
+++ frontend/home.php
@@ -16,6 +16,7 @@ include "libs/load.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link href="css/home.css" rel="stylesheet">
     <script src="js/home.js"></script>
   </head>
@@ -24,6 +25,63 @@ include "libs/load.php";
 <body>
     <?php load_temp("header.php"); ?>
     <div class="container">
-      
+      <div class="card1">
+          <div id="carouselExample" class="carousel slide adj">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="resource/im1.jpeg" class="d-block w-100 h-60" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="resource/im1.jpeg" class="d-block w-100 h-60" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="resource/im3.jpeg" class="d-block w-100 h-60" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
+
+
+      <div class="card adj1">
+    <h5 class="card-header">Complients</h5>
+    <div class="card-body">
+      <h5 class="card-title">Public quries</h5>
+      <p class="card-text">Addressing public queries related to green awareness is vital for fostering a sense of environmental responsibility, empowering individuals and businesses to make environmentally conscious decisions, and collectively contributing to a more sustainable and eco-friendly future. Providing accurate and accessible information can help inspire positive change and encourage people to become advocates for the environment</p>
+      <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">Click here</button>
+    </div>
+    </div>
+
+      
+
+       <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalCenterTitle">Complient</h5>
+              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form action="#" method="POST">
+                
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div>
+  </div>
+  <?php load_temp("footer.php"); ?>
 </body>
---
+++ frontend/templates/footer.php
@@ -0,0 +1,21 @@
+<head>
+    <link href="css/footer.css" rel="stylesheet">
+ </head>
+<footer class="container navbar navbar-expand-md custom-common-footer fixed-bottom">
+    <div class="container custom-footer">
+        <div class="navbar-brand">
+        <a class="nav-link" style="font-size:15px">© 2023 TODO-INC</a>
+        </div>
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">About</i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="calender.php">Upgrades</i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Team</i></a>
+          </li>
+        </ul>
+    </div>
+</footer>
