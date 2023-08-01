b7f68820a0f45cccc601e6273631febc227fb4b5
saran-raj007 <k.saranraj000@gmail.com>
2023-08-01 19:05:44 +0530

    home page update

--- frontend/templates/footer.php
+++ frontend/templates/footer.php
@@ -1,21 +1,19 @@
 <head>
     <link href="css/footer.css" rel="stylesheet">
  </head>
-<footer class="container navbar navbar-expand-md custom-common-footer fixed-bottom">
-    <div class="container custom-footer">
-        <div class="navbar-brand">
-        <a class="nav-link" style="font-size:15px">© 2023 TODO-INC</a>
-        </div>
-        <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="index.php">About</i></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="calender.php">Upgrades</i></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">Team</i></a>
-          </li>
-        </ul>
+ <div class="container chan fixed-bottom">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 TODO, Inc</span>
     </div>
-</footer>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+    </ul>
+  </footer>
+</div>
