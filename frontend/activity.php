8fa171b9e86fac547829621fdece82325f3c497c
SUMUGAR  M <sumugar001@gmail.com>
2023-08-03 07:22:53 +0000

    act

---
+++ frontend/activity.php
@@ -0,0 +1,45 @@
+<?php
+include "libs/load.php";
+?>
+<head>
+  <!doctype html>
+  <html lang="en" data-bs-theme="auto">
+    <head>
+      <script src="vendor/assets/js/color-modes.js"></script>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <meta name="generator" content="Hugo 0.112.5">
+      <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+      <link href="vendor/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+      <script src="vendor/assets/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <link href="css/home.css" rel="stylesheet">
+      <script src="js/home.js"></script>
+    </head>
+</head>
+
+  <body>
+    <?php load_temp("header.php"); ?>
+      <div class="container"style="background-color: #D8D9DA;border-radius: 20px;">
+        <div class="row"style="color:">
+          <div class="card">
+            <div class="card-">
+              <h1><div class="card-title">Title</div></h1>
+              <h3><div class="card-subtitle">Subtitle</div></h3>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, doloribus sunt. Illo, ea tenetur rem provident vero, unde laudantium, molestias voluptatem doloremque aperiam inventore fuga? Minus eaque nesciunt unde. Dolores!
+            </div>
+          </div>
+          <!-- qwerty -->
+        </div>
+      </div>
+       
+
+
+
+    <?php load_temp("footer.php"); ?>
+  </body>
+</html>
