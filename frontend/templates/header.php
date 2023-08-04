efb08c63e17d625cbf9136977d46e8a6f7fce8e4
saran-raj007 <k.saranraj000@gmail.com>
2023-08-04 16:42:17 +0530

    update

--- frontend/templates/header.php
+++ frontend/templates/header.php
@@ -1,4 +1,5 @@
  <head>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
     <link href="css/header.css" rel="stylesheet">
@@ -7,7 +8,7 @@
     <header class="neww">
       <!--// NAV BAR //-->
      
-      <div class="container check fixed-top">
+      <div class="container check">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
           <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <div class="circular-image">
@@ -22,11 +23,19 @@
             <li class="nav-item"><a href="#" class="nav-link">Air</a></li>
             <li class="nav-item"><a href="#" class="nav-link">water</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Activites</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+            <h1 class="dropdown ">
+           <button class="btn clr" type="button" id="headingDropdown" data-bs-toggle="dropdown" aria-expanded="false">others</button>
+              <div class="dropdown-menu" aria-labelledby="headingDropdown">
+                  <a class="dropdown-item" href="querydetails.php">Querydetails</a>
+                  <a class="dropdown-item" href="#">contact</a>
+                  <a class="dropdown-item" href="#">Wast</a>
+              </div>
+            </h1>
           </ul>
         </header>
       </div>         
       </div> 
+      <h1 class="d-flex justify-content-center cent">"Let's make Madurai green and clean."</h1>
       <div class="down">
          
          <a class="next-page" href="#about-section"><i class="animated pulse infinite fa fa-chevron-circle-down fa-2x" aria-hidden="true"></i></a>
