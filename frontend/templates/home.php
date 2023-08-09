3d76f0f43c9753719d51604857164f8f6ace76f8
saran-raj007 <k.saranraj000@gmail.com>
2023-08-09 16:00:13 +0530

    add bot

--- frontend/templates/home.php
+++ frontend/templates/home.php
@@ -156,7 +156,17 @@ include "libs/load.php";
             </div>
         </div>
       <div>
-    </div>  
+    </div> 
+    <script type="text/javascript">
+      window.__be = window.__be || {};
+      window.__be.id = "64d0f9b9d01f120007af4c8e";
+      (function() {
+          var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+          be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+      })();
+  </script>
+  <noscript>You need to <a href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/" rel="noopener nofollow">enable JavaScript</a> in order to use the AI chatbot tool powered by <a href="https://www.chatbot.com/" rel="noopener nofollow" target="_blank">ChatBot</a></noscript> 
     {% include "footer.php"%}
 </body>
 </html>
